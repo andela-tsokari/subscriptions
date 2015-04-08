eba74b9323566d205cfae0dd86c492af0ebd69c8
Tams Sokari <tams.sokari@andela.co>
2015-04-08 17:11:20 +0100

    fix model schema

--- app/models/subscription.model.js
+++ app/models/subscription.model.js
@@ -1,18 +1,18 @@
 //require mongoose module
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 //require mongoose Schema creator
 var Schema = mongoose.Schema;
 
 //create subscription Schema
 var subscriptionSchema = new Schema({
   post: {
-    title: {type: String, required: true, unique: true},
-    comments: {type: Boolean, required: true, default: true},
+    title: {type: String, required: true,default: "Not a Post Subscription"},
+    comments: {type: Boolean, required: true, default: false},
     edits: {type: Boolean, required: true, default: false}
   },
   tag: {
-    title: {type: String, required: true, unique: true},
-    posts: {type: Boolean, required: true, default: true},
+    title: {type: String, required: true, default: "Not a Tag Subscription"},
+    posts: {type: Boolean, required: true, default: false},
     comments: {type: Boolean, required: true, default: false}
   }
 });
