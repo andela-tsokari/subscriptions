c28039e4335fc2ad4dc296b6c28afbd437b31144
Tams Sokari <tams.sokari@andela.co>
2015-04-08 12:24:11 +0100

    create database config for development, testing (local and remote) and production

---
+++ config/db-config.js
@@ -0,0 +1,19 @@
+module.exports = (function () {
+  'use strict';
+  return {
+    db: {
+      development: {
+        uri: 'mongodb://localhost/subscriptions'
+      },
+      test: {
+        uri: 'mongodb://localhost/subscriptions-test'
+      },
+      staging: {
+        uri: 'process.env.MONGOLAB_URI'
+      },
+      production: {
+        uri: 'process.env.MONGOLAB_URI'
+      }
+    }
+  };
+})();
