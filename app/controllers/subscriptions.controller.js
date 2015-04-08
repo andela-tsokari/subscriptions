3488495413477fbd0c3d1ccf89471058b94f71ba
Tams Sokari <tams.sokari@andela.co>
2015-04-08 12:59:51 +0100

    create controller for api functions. get all and create new added so far

---
+++ app/controllers/subscriptions.controller.js
@@ -0,0 +1,28 @@
+var SubscriptionModel = require('..app/models/subscription.model');
+
+module.exports = {
+  getAllSubscriptions: function(request, response){
+    SubscriptionModel.find(function(error, subscriptions){
+      if (error) {
+        response.json(error);
+      }
+      else {
+        response.json(subscriptions);
+      }
+    });
+  },
+
+  createSubscription: function(request, response) {
+    var subscription = new SubscriptionModel(request.body);
+    subscription.save(function(error){
+      if (error) {
+        response.json(error);
+      }
+      else {
+        response.json({message: 'New Subscription Added'});
+      }
+    });
+
+
+  }
+};
