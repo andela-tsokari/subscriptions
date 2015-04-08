f4f96763ee5249cc4b737ad03a76dc6a2687e9d7
Tams Sokari <tams.sokari@andela.co>
2015-04-08 17:10:52 +0100

    fix router issue

--- config/app-config.js
+++ config/app-config.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var routes = require('./../app/routes/subscriptions.route');
-var app = express();
-var router = express.Router();
-app
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({extended : true}))
-  .use('/', router);
-routes(router);
 
-module.exports = app;
+module.exports = function() {
+  var app = express();
+  var router = express.Router();
+  app
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({extended : true}))
+    .use('/', router);
 
+  routes(router);
+
+  return app; 
+};
