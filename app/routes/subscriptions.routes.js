63e23a769842549ccaf66b34f8f39b4a00e730e8
Tams Sokari <tams.sokari@andela.co>
2015-04-08 13:40:59 +0100

    rename file to reflect purpose

--- app/routes/subscriptions.routes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var subscriptions = require('../controllers/subscription.controller');
-var router = express.Router();
-
-router.route('/subscriptions')
-  .get(subscriptions.getAllSubscriptions)
-  .post(subscriptions.createSubscription
-  );
