65a55c5f668921dd9d44045e22218c80e00bce11
Tams Sokari <tams.sokari@andela.co>
2015-04-09 15:59:59 +0100

    add get, update and delete for a single subscription

--- app/controllers/subscriptions.controller.js
+++ app/controllers/subscriptions.controller.js
@@ -23,5 +23,68 @@ module.exports = {
       }
     });
 
+  },
+
+  getOneSubscription: function(request, response){
+    var id = request.params._id;
+    SubscriptionModel.findById(id, function(error, subscriptions){
+      if (error) {
+        response.json(error);
+      }
+      else {
+        response.json(subscriptions);
+      }
+    });
+  },
+
+  deleteOneSubscription: function(request, response){
+    var id = request.params._id;
+    SubscriptionModel.findByIdAndRemove(id, function(error, subscriptions){
+      if (error) {
+        response.json(error);
+      }
+      else {
+        response.json({message: "your subscription was deleted."});
+      }
+    });
+  },
+
+  updateOneSubscription: function(request, response){
+    //split up required parts of the request body.
+    var id = request.params._id;
+    var edit = request.body;
+    SubscriptionModel.findByIdAndUpdate(id, edit, function(error, subscriptions){
+      if (error) {
+        response.json(error);
+      }
+      else {
+        response.json({message: "your subscription was successfully updated"});
+      }
+    });
+  },
+
+  getPostSubscriptions: function(request, response){
+    var id = request.params.posts;
+    SubscriptionModel.find(id, function(error, subscriptions){
+      if (error) {
+        response.json(error);
+      }
+      else {
+        response.json(subscriptions);
+      }
+    });
+  },
+
+  getTagSubscriptions: function(request, response){
+    var id = request.params.tags;
+    SubscriptionModel.find(id, function(error, subscriptions){
+      if (error) {
+        response.json(error);
+      }
+      else {
+        response.json(subscriptions);
+      }
+    });
   }
+
 };
