b6aa36cd5c0a2a2bcbdc39e061c6cd9c77e92010
Tams Sokari <tams.sokari@andela.co>
2015-04-13 13:12:01 +0100

    comment code

--- app/controllers/subscriptions.controller.js
+++ app/controllers/subscriptions.controller.js
@@ -1,6 +1,9 @@
+//require the schema that was exported via the model. its methods are to be used here
 var SubscriptionModel = require('./../models/subscription.model');
 
+//all methods created here, are to be sent to the routes. export them as a module
 module.exports = {
+  //method to retrieve all subscriptions, 
   getAllSubscriptions: function(request, response){
     SubscriptionModel.find(function(error, subscriptions){
       if (error) {
@@ -11,7 +14,7 @@ module.exports = {
       }
     });
   },
-
+  //method to create a new subscription
   createSubscription: function(request, response) {
     var subscription = new SubscriptionModel(request.body);
     subscription.save(function(error){
@@ -24,7 +27,7 @@ module.exports = {
     });
 
   },
-
+  //method to get one subscription.
   getOneSubscription: function(request, response){
     var id = request.params._id;
     SubscriptionModel.findById(id, function(error, subscriptions){
@@ -50,7 +53,7 @@ module.exports = {
   },
 
   updateOneSubscription: function(request, response){
-    //split up required parts of the request body.
+    //to update a subscription, we need to use the findByIdAndUpdate method. This will first find the unique subscription, then insert the required edit. The edit will be the request body.
     var id = request.params._id;
     var edit = request.body;
     SubscriptionModel.findByIdAndUpdate(id, edit, function(error, subscriptions){
@@ -62,27 +65,46 @@ module.exports = {
       }
     });
   },
-
+  //method to get all post subscriptions
   getPostSubscriptions: function(request, response){
-    var id = request.params.posts;
-    SubscriptionModel.find(id, function(error, subscriptions){
+    //post subscriptions can be gotten by excluding the subscriptions with default post values. to do this, we use the mongoDB query method '$ne' to exclude all default post values. then we return all posts stored.
+    var posts = {"post": { 
+                            $ne: {
+                            //the sequence in which the fields are entered should follow the sequence, the database saves the fields as. 
+                                    "edits": false, 
+                                    "comments": false, 
+                                    "title": "Not a Post Subscription" 
+                                  } 
+                          } 
+                };
+    SubscriptionModel.find(posts, function(error, posts){
       if (error) {
         response.json(error);
       }
       else {
-        response.json(subscriptions);
+        response.json(posts);
       }
     });
   },
 
+  //method to get all tag subscriptions
   getTagSubscriptions: function(request, response){
-    var id = request.params.tags;
-    SubscriptionModel.find(id, function(error, subscriptions){
+    //tag subscriptions can be gotten by excluding the subscriptions with default tag values. to do this, we use the mongoDB query method '$ne' to exclude all default tag values. then we return all tags stored.
+    var tags = {"tag": { 
+                          $ne: { 
+                            //the sequence in which the fields are entered should follow the sequence, the database saves the fields as. 
+                                  "comments": false, 
+                                  "posts": false, 
+                                  "title": "Not a Tag Subscription" 
+                                } 
+                        } 
+                };
+    SubscriptionModel.find(tags, function(error, tags){
       if (error) {
         response.json(error);
       }
       else {
-        response.json(subscriptions);
+        response.json(tags);
       }
     });
   }
