d08aa9d4b63696a3d92a58ba91aa6c45956be6b7
Tams Sokari <tams.sokari@andela.co>
2015-04-21 16:48:04 +0100

    modify routes to signify the inclusion of a user, also require cors for route restrictions

--- app/routes/subscriptions.route.js
+++ app/routes/subscriptions.route.js
@@ -1,12 +1,13 @@
 //the functionality which the routes have is stored in the controller. thus, the first step is to require the controller.
 var subscriptions = require('./../controllers/subscriptions.controller');
 
+var cors = require('cors');
+
 //we will export this router capability to the server.js location. so it can be used by the app
 module.exports = function (router) {
   //route for all subscriptions. from here, we can create new posts and retrieve all old posts.
   router.route('/subscriptions')
-    .get(subscriptions.getAllSubscriptions)
-    .post(subscriptions.createSubscription);
+    .get(subscriptions.getAllSubscriptions);
   //route for post subscriptions. we only need the retrieve ability here.
   router.route('/subscriptions/posts')
     .get(subscriptions.getPostSubscriptions);
@@ -15,7 +16,25 @@ module.exports = function (router) {
     .get(subscriptions.getTagSubscriptions);
   //route for unique subscriptions. the colon( : ) before shows the item in that location is variable. unique subscriptions can be retrieved, deleted or have their status updated.
   router.route('/subscription/:_id')
-    .delete(subscriptions.deleteOneSubscription)
-    .put(subscriptions.updateOneSubscription)
     .get(subscriptions.getOneSubscription);
+
+  router.route('/:username/subscriptions')
+    .get(subscriptions.getUserSubscriptions);
+
+  router.route('/:username/subscriptions/posts')
+    .get(subscriptions.getUserPostSubscriptions);
+
+  router.route('/:username/subscriptions/tags')
+    .get(subscriptions.getUserTagSubscriptions);
+
+  router.route('/:username/new-subs')
+    .post(cors(), subscriptions.createUserSubscription);
+
+  router.route('/:username/subscription/:_id')
+    .get(subscriptions.getUserSubscription);
+
+  router.route('/:username/subscription/:_id')
+    .put(cors(), subscriptions.editUserSubscription)
+    .delete(cors(), subscriptions.deleteUserSubscription);
+
 };
